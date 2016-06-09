d642786a461c3ca903a5175bc069b6ae32c71844
AnishaSabu <annrosepaul4@gmail.com>
2016-06-09 15:07:34 +0530

    program to load and show an image

--- loading_image.py
+++ loading_image.py
@@ -1,6 +1,5 @@
 import cv2 #this will immport opencv library
 cv2.namedWindow('image',cv2.WINDOW_AUTOSIZE)
 image = cv2.imread("im1.jpg") #this will read an image from directory and we get an object of that source image called "sample.png" as image 
-cv2.resize(image,(500,500))
 cv2.imshow("image",image)
 cv2.waitKey(0)
